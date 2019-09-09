9177b0b3a89c31a5bf0f155570383cb1530e10a6
Tyler Thompson <30381145+TThompson2001@users.noreply.github.com>
2019-09-08 23:18:54 -0400

    Update PersonReader.java

--- src/PersonReader.java
+++ src/PersonReader.java
@@ -1,120 +1,48 @@
+import javafx.stage.FileChooser;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.io.*;
 import java.util.*;
-import java.text.*;
+public class PersonReader {
 
-        public class PortfolioGenerator {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        try {
+            // JFile code
+            JFileChooser chooser = new JFileChooser("classmates.csv");
 
+            int returnValue = chooser.showOpenDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = chooser.getSelectedFile();
 
-                public static void main (String[]args){
-
-                    ArrayList<String> pList = new ArrayList();
-                    ArrayList<String> Ach = new ArrayList();
-                    SafeInput sI = new SafeInput();
-
-
-
-                    Boolean marker = true;
-                    Boolean markerTwo = true;
-
-                    Scanner scanner = new Scanner(System.in);
-
-                    // These are just the numbers for when the input is printed to the file
-                    int i = 1;
-                    int b = 1;
+                BufferedReader objReader = new BufferedReader(new FileReader(selectedFile));
+                String line;
 
 
-                    String email = "";
-                    String lang = "";
-                    String aAndI = "";
-
-                    // Name Entry
-                    System.out.println("What's your name?");
-                    String name = scanner.nextLine();
-
-                    // Email Entry
-                    System.out.println("What's your email");
-                    email = scanner.nextLine();
-                    sI.getEmail(email);
-
-                    // Email Verification in SafeInput EXTRA CREDIT
-                    while (!sI.getEmail(email)) {
-                        System.out.println("Invalid Response: Retry");
-                        email = scanner.nextLine();
-                    }
-
-                    System.out.println("Can you give some of your personal background?");
-                    String personal = scanner.nextLine();
-
-                    // Personal Info Validation
-                    while (!sI.getString(personal)) {
-                        System.out.println("You didn't write enough! Try again!");
-                        personal = scanner.nextLine();
-                    }
-
-
-                    // Loop for multiple answers
-                    while (marker) {
-                        System.out.println("Programming Languages");
-                        lang = scanner.next();
-                        pList.add(new String(lang));
-                        System.out.println("Is there any more? Y/N");
-                        Character answer = scanner.next().charAt(0);
-                        if (answer == 'N') {
-                            marker = false;
+                boolean first = true;
+                // run and print
+                while ((line = objReader.readLine()) != null) {
+                    String[] info = line.split(",");
+                    int numofitems = info.length;
+                    for (int i = 0; i < numofitems; i++) {
+                        String column = info[i];
+                        if (first) {
+                            System.out.printf("%-20s", column.toUpperCase());
+                        } else {
+                            System.out.printf("%-20s", column);
                         }
                     }
-                    System.out.println(pList);
-
-
-                    System.out.println(pList);
-                    while (markerTwo) {
-                        System.out.println("Achievements and interests you would like to share");
-                        aAndI = scanner.next();
-                        Ach.add(scanner.next());
-                        System.out.println("More Y/N");
-                        Character answerTwo = scanner.next().charAt(0);
-                        if (answerTwo == 'N') {
-                            markerTwo = false;
-                        }
-                    }
-
-                        // Write to file
-                        try {
-                            FileWriter fw = new FileWriter("outputFile.txt");
-                            PrintWriter pw = new PrintWriter(fw);
-
-                            pw.println("***************************");
-                            pw.println(name);
-                            pw.println("***************************");
-                            pw.println(" ");
-                            pw.println("E-mail: " + email);
-                            pw.println(" ");
-                            pw.println("** Personal Background:");
-                            pw.println(personal);
-                            pw.println("** Programming Languages:");
-                            pw.println("---------------------------");
-                            for (String s : pList) {
-
-                                pw.println(i + ". " + s);
-                                i++;
-                            }
-                            pw.println();
-                            pw.println("** Achievements and interests:");
-                            pw.println("-------------------------------");
-                            for (String a : Ach) {
-
-                                pw.println(b + ". " + a);
-                                b++;
-
-
-                            }
-                                pw.close();
-                        } catch (IOException e) {
-                        }
-
+                    System.out.println("  ");
+                    first = false;
                 }
             }
 
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
+}
